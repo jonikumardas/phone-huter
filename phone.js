e0d1f8414bcb062eef348d440ade2dc9b221d833
jonikumardas <jonikumardas213@gmail.com>
2022-03-02 16:46:36 +0600

    mobile phone show on display

--- phone.js
+++ phone.js
@@ -16,3 +16,32 @@ const inputFieldValue = () => {
     .then(data =>displaydata(data.data.slice(0,20)));
   }
 }
+// function for 1st data append 
+const displaydata = phones => {
+    if (phones == 0) {
+      const error = document.getElementById("error-massage2");
+      error.style.display = "block"
+    } else {
+      const error = document.getElementById("error-massage2");
+      error.style.display = "none";
+      error.style.display = "";
+     const displayCard = document.getElementById("container");
+     displayCard.textContent = "";
+      phones.forEach(phones => {
+          const div = document.createElement("div")
+          div.classList.add("col");
+          div.innerHTML = `
+          <div class="card border border-dark rounded-3 shadow-lg">
+          <img src="${phones.image}" class="card-img-top w-75 mx-auto pt-3" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Name: ${phones.phone_name}</h5>
+            <p class="card-text"> <h5>Brand:${phones.brand}</h5></p>
+            <button onclick="datailsvalue('${phones.slug}')" type="button" class="btn btn-info">details</button>
+          </div>
+          
+        </div>
+        `;
+        displayCard.appendChild(div);
+      });
+    }
+}
